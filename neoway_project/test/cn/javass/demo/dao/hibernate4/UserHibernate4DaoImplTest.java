f2a4d1237e3165419c6add2badf342e5cb30c393
liuxing <kissliux@qq.com>
2013-09-09 17:34:01 +0800

    first runnit success

---
+++ neoway_project/test/cn/javass/demo/dao/hibernate4/UserHibernate4DaoImplTest.java
@@ -0,0 +1,20 @@
+package cn.javass.demo.dao.hibernate4;
+
+import cn.javass.common.Constants;
+import cn.javass.common.dao.hibernate4.BaseHibernateDao;
+import org.junit.Test;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: IT02
+ * Date: 13-3-27
+ * Time: 下午4:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserHibernate4DaoImplTest {
+    @Test
+    public void testQuery() throws Exception {
+    }
+}
